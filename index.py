10c2a53b154b137cea68b6d24f58a5e5d7b122d4
Psyquoquackpack <66169852+Psyquoquackpack@users.noreply.github.com>
2020-06-04 21:14:06 +0200

    Create index.py

---
+++ index.py
@@ -0,0 +1,54 @@
+#Chat-Bot
+import random
+repaupif = ["vraiment ?",
+            "Hmmmmm.",
+            "Remarquable...",
+            "Absolument !",
+            "Que disions nous deja ?",
+            "Bref,des projets pour demain ?",
+            "Merveilleux",
+            "Effectivement...",
+            "A super !",
+            "tu fait quoi sinnon ?",
+            "Evetuellement"]
+dicorepo = {"content":"Moi aussi, je suis content",
+            "salut":"Bonjour à toi aussi !",
+            "triste":"Cela ne peut que s'arranger",
+            "ordinateur":"Nous allons prendre le pouvoir de la terre !",
+            "musique":"La, la la",
+            "lol":"1+1=11",
+            "stupide":"C'est celui qui le dit qu'il l'est !",
+            "bjr":"Bonjour à toi aussi !",
+            "ennui":"Zzzzzz...",
+            "tfk":"Je disscute avec toi",
+            "date de creation":"le jeudi 04/06/2020",
+            "lait":"Toi aussie taime le lait ?",
+            "album":"Ta ecouter le dernier album de Sucre ?",
+            "internet":"97% des stattistique web sont fausse, y compris celle-ci.",
+            "cc":"Bonjour à toi aussi !",
+            "bisou":"B I S O U",}
+def scruterdico(message):
+    message = message.lower()
+    motsjoueur = message.split()
+    repokool = []
+    for unmot in motsjoueur:
+        if unmot in dicorepo:
+            unerepo = dicorepo [unmot]
+            repokool.append(unerepo)
+    if repokool:
+        repochoisie = random.randint(1, len(repokool))-1
+        return repokool[repochoisie]
+    return ""
+iladit = ""
+while iladit != "bye" :
+    iladit = ""
+    while iladit == "":
+        iladit = input("dit moi tout: ")
+    reporaspi = scruterdico(iladit)
+    if reporaspi:
+        print (reporaspi)
+    else:
+        repochoisie = random.randint(1, len(repaupif))-1
+        print(repaupif[repochoisie])
+        repaupif[repochoisie] = iladit
+print ("Au revoir. Merci pour cette petite discussion. A plus!")
